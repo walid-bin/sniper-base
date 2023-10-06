864bc1ada60b732f4967256bd093b4d185eaa305
walid-bin <jashim@coredevs.ltd>
2023-10-06 11:55:11 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 const { Web3Ins } = require("./webThree");
-
+const shareRange = require("../shareRange.json");
 const key = process.env.PRIVATE_KEY;
 const webThree = new Web3Ins(key);
 
-// webThree.getBalanceOf("0x172F7E333a2B812DfD499eFD0E390736dCaa114B").then(res => res)
 
+const limit = Number(process.env.LIMIT);
 const cont = webThree.getContract();
 
-cont.events.allEvents({}, () => { }).on('data', async (data) => {
-    // webThree.getBalanceOf(data.returnValues.trader).then(res => console.log(webThree.web3.utils.fromWei(res, 'ether'), data.returnValues.isBuy, data.returnValues.ethAmount, data.transactionHash));
+cont.events.Trade({}, () => { }).on('data', async (data) => {
     const response = {
         isBuy: data.returnValues.isBuy,
         transactionHash: data.transactionHash,
         ref: data.returnValues.subject,
         trader: data.returnValues.trader,
     };
+    const balanceOf = await webThree.getBalanceOf(data.returnValues.trader);
     const resp = await webThree.web3.eth.getTransaction(data.transactionHash);
-    response.value = resp.value;
-    console.log(response);
-    /* 
-    the respnse contains the refer address and trader address, and also have transection hash and the caller of the event is buy or not. 
-    now you have to decide the action
-    */
+    response.value = webThree.web3.utils.fromWei(resp.value, 'ether');
+    response.balanceOf = webThree.web3.utils.fromWei(balanceOf, 'ether');
+
+
+    // console.log(response.transactionHash, response.balanceOf);
+
+    /*
+     this is the logic to find the share count of the buy order action 
+    console.log(shareRange.find((range) => ((Number(range.min) < Number(response.balanceOf)) && (Number(range.max) > Number(response.balanceOf)))), response.balanceOf);
+ */
+
 });
